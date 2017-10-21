614c5b753dfbf170cf88fcbc8afe2eeb1e31cbec
Antonio Augusto DE CINTRA BATISTA <313526+aacb@users.noreply.github.com>
2017-10-21 20:09:34 -0200

    Add files via upload

---
+++ usr/lib/fbhto/fbhto-lib.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+####
+##  fbhto-lib.sh
+##
+##  Rotinas específicas do fbhto - From black hole to organized
+##  Estas rotinas implementam o que fazer com cada tipo de arquivo
+##  colocado no buraco negro.
+##
+##  (c) 2017 by Antonio Augusto de Cintra Batista <antonio.a.c.batista@gmail.com>
+##  Licensed under GNU GPL v3
+##
+##  2017-10-13: first version
+##
+####
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, version 3 of the License.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program.  If not, see http://www.gnu.org/licenses/
+##
+####
+regExpImage='bmp$|fpx$|gif$|j2c$|j2k$|jfif$|jif$|jp2$|jpe?g$|jpx$|pbm$|pcd$|pgm$|png$|pnm$|ppm$|svg$|tif$|tiff$'
+regExpVideo='avi$|dhmov$|dvx$|f4v$|flv$|gifv$|gvi$|gvp$|h264$|hdv$|ivr$|ivs$|m15$|m1v$|m21$|m2ts?$|m2v$|m4e$|m4v$|m75$|mgv$|mjp$|mjpe?g$|mk3d$|mkv$|mob$|mov$|movie$|mp21$|mp2v$|mp4$|mp4v$|mpe$|mpe?g$|mpeg[124]$|mpg[24]$|og[mvx]$|qtm?$|rm$|rv$|vid$|vob$|vro$|webm$|wm$|wmv$|wvm$|xvid$|3gp$'
+regExpOffice='docx?$|odp$|ods$|odt$|pdf$|pptx?$|ps$|rtf$|sxw$|tex$|wp$|wp[d7]$|xlsx?$'
+regExpText='te?xt$|asc$'
+
+
+extensaoDoArquivo() {
+  local arquivo="$1"
+  extensao=''
+  if [[ "$arquivo" =~ \. ]]; then
+    extensao="${arquivo##*.}"
+    extensaoParaComparacao=$(echo "$extensao" |tr '[:upper:]' '[:lower:]')
+  fi
+}
+
+recuperaMetatag() {
+  local arquivo="$1"
+  metaTag=$(exiftool -d %Y-%m-%d_%H.%M.%S "$arquivo" |grep -i date |grep '[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}_[0-9]\{2\}.[0-9]\{2\}.[0-9]\{2\}' |awk -F ":" '{ print $2 }' |sort -n |head -1)
+}
+
+extraiFileTime() {
+  fileTime=""
+  if [[ "$metaTag" != "" ]]; then
+    fileTime=$(echo "$metaTag" |sed -e 's#^.*\([1-9][0-9]\{3\}-[0-9]\{2\}-[0-9]\{2\}\_[0-9]\{2\}.[0-9]\{2\}.[0-9]\{2\}\).*$#\1#g')  # example: 2017-10-13
+echo oie
+  fi
+}
+
+decideOndeColocar() {
+  local arquivo="$1"
+  if   [[ $extensaoParaComparacao =~ $regExpImage ]]; then
+    pastaParaExtensao="image"
+  elif [[ $extensaoParaComparacao =~ $regExpOffice ]]; then
+    pastaParaExtensao="office"
+  elif [[ $extensaoParaComparacao =~ $regExpText ]]; then
+    pastaParaExtensao="text"
+  elif [[ $extensaoParaComparacao =~ $regExpVideo ]]; then
+    pastaParaExtensao="video"
+  else
+    pastaParaExtensao="other-file-types"
+  fi
+  nomeDoArquivo=$(basename "$arquivo" |sed -e 's#\(.*\)\.'$extensao'#\1#g')
+  if [[ "$fileTime" != "" ]]; then
+    destinoFinal="$pastaDestino"'/'$pastaParaExtensao'/'${fileTime:0:4}'/'${fileTime:5:2}'/'${fileTime:8:2}
+    [ -d "$destinoFinal" ] || mkdir -p "$destinoFinal"
+    arquivoNoDestino="$destinoFinal"'/'$fileTime'-'"$nomeDoArquivo"
+  else
+    arquivoNoDestino="$pastaDestinoIncerto""/""$nomeDoArquivo"
+  fi
+  if [[ "$extensao" != "" ]]; then
+    arquivoNoDestino="$arquivoNoDestino"'.'"$extensao"
+  fi
+  fNesteNivelDeDebugEscrever 2 "arquivoNoDestino: ""$arquivoNoDestino"
+  comando='mv -f "'"$arquivo"'" "'"$arquivoNoDestino"'"'
+  # move the file to its organized place
+  echo "decideOndeColocar: ""$comando"
+  echo
+  eval "$comando"
+}
+
+posProcessa() {
+  local arquivoNoDestino="$1"
+}
+
---
+++ usr/sbin/fbhto.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+################
+## fbhto.sh ##
+################
+
+####
+##  fbhto
+##  From black hole to organized
+##  
+##  Script para monitorar, em tempo real, o aparecimento
+##  de arquivos numa pasta de entrada, também chamada de
+##  buraco negro ou pasta da bagunça, e para transferir este
+##  arquivo de forma organizada para a sua devida pasta e com
+##  um nome que ajude na organização e recuperação posterior
+##  dos arquivos.
+##
+##  (c) 2017 by Antonio Augusto de Cintra Batista <antonio.a.c.batista@gmail.com>
+##  Licensed under GNU GPL v3
+##  Version 0.2 - 2017-10-08
+##
+####
+
+source /usr/lib/fbhto/common-lib.sh
+source /usr/lib/fbhto/fbhto-lib.sh
+
+###
+# read the configuration files
+###
+# regular expression: expected configuration line sintax
+regExpConf='^[a-zA-Z0-9\._-]+=[a-zA-Z0-9\.\$/_"-]'
+numLinha=0
+while read linha; do
+  ((numLinha++))
+  linha=$(echo "$linha" |sed -e 's#^[[:space:]]*##g' |sed -e 's/^\(.*\)#.*$/\1/g' |sed -e 's#[[:space:]]*$##g')
+  if [[ "${linha:0:1}" != "#" && "${linha:0:1}" != "" ]]; then
+  # skip comments
+    if [[ "$linha" =~ $regExpConf ]]; then
+    # conf line will be accepted
+      fNesteNivelDeDebugEscrever 7 "linha $numLinha: ""$linha"
+      eval "$linha"
+    else
+      fNesteNivelDeDebugEscrever 0 "SINTAX ERROR found at line $numLinha of /etc/fbhto/fbhto.conf"
+      fNesteNivelDeDebugEscrever 0 "$linha"
+      exit 1
+    fi
+  fi
+done </etc/fbhto/fbhto.conf
+
+monitoraPasta() {
+  # inotify events: modify attrib moved_to moved_from move_self create delete delete_self unmount
+  local pasta="$1"
+  houveInterrupcao=0
+  trap fechaTudo SIGHUP SIGINT SIGTERM SIGQUIT
+  while read linha; do
+    trap vaiMasVolta SIGHUP SIGINT SIGTERM SIGQUIT
+    fNesteNivelDeDebugEscrever 2 "linha gerada pelo inotifywait: ""$linha"
+    agora=$(date +%Y-%m-%d_%H.%M.%S-%s)
+    arquivo=$(echo "$linha" |awk -F ";" '{ print $2 }')
+    fNesteNivelDeDebugEscrever 7 "linha gerada pelo inotifywait: ""$linha"
+    fNesteNivelDeDebugEscrever 7 "arquivo que chegou ao bh: ""$arquivo"
+    if [[ "$pastaBh""/" != "$arquivo" ]]; then
+      extensaoDoArquivo "$arquivo"
+      recuperaMetatag "$arquivo"
+      extraiFileTime
+      decideOndeColocar "$arquivo"
+      posProcessa "$arquivoNoDestino"
+      # echo "$agora"";""$arquivo"" transferido para ""$pastaDestino" >> "$arquivoDeLog"
+    fi
+    [[ $houveInterrupcao == 1 ]] && fechaTudo
+    trap fechaTudo SIGHUP SIGINT SIGTERM SIGQUIT
+  done < <(inotifywait -m -r -e close_write "$pasta" --format "%e;%w%f;" --quiet --fromfile "$arquivoComListaDeExclusao" --excludei "tmp$")
+  trap fechaTudo SIGHUP SIGINT SIGTERM SIGQUIT
+}
+
+apresentacao() {
+  printf "\nEste script irá monitorar, em tempo-real, a pasta %s\n\nCaso algum arquivo colocado nesta pasta, ele será transferido imediatamente para a pasta %s, caso seja possível organizá-lo, ou para a pasta %s, caso este aplicativo ainda não saiba como fazer para o organizar.\n\nOs logs correspondentes serão gerados e deverão estar disponíveis para consulta no arquivo:\n\t%s\n\n" "$pastaBh" "$pastaDestino" "$pastaDestinoIncerto" "$arquivoDeLog"
+}
+
+##############
+####      ####
+###  main  ###
+####      ####
+##############
+
+pastaDeConfiguracao="/etc/fbhto/"
+
+# testa se as pastas existem
+[ -d "$pastaDeConfiguracao" ] || { mkdir -p "$pastaDeConfiguracao" ;touch "$arquivoComListaDeExclusao" ;}
+[ -e "$arquivoComListaDeExclusao" ] || touch "$arquivoComListaDeExclusao"
+[ -d "$pastaBh" ] || { mkdir -p "$pastaBh" ;}
+[ -d "$pastaDestino" ] || { mkdir -p "$pastaDestino" ;}
+[ -d "$pastaDestinoIncerto" ] || mkdir -p "$pastaDestinoIncerto"
+[ -d "$pastaBaseDeLog" ] || mkdir -p "$pastaBaseDeLog"
+
+if [ -e "$pastaBh" ]; then
+  if [ ! -d "$pastaBh" ]; then
+    echo
+    echo "ERRO!!! Favor verificar:"  >2
+    echo "$pastaBh"" existe e deveria ser um diretório, mas não é." >2
+    echo "ERRO!!! Favor verificar:" >>"$arquivoDeLog"
+    echo "$pastaBh"" existe e deveria ser um diretório, mas não é." >>"$arquivoDeLog"
+    echo
+    exit 1
+  fi
+else
+  mkdir -p "$pastaBh"
+fi
+
+apresentacao
+
+# seta permissões, owner e group
+chown -R www-data:www-data "$pastaBh"
+while IFS= read -d $'\0' -r arq ;do
+  [ -d "$arq" ] && chmod 755 "$arq"
+  [ -f "$arq" ] && chmod 644 "$arq"
+done < <(find "$pastaBh" -print0)
+
+trap fechaTudo SIGHUP SIGINT SIGTERM SIGQUIT
+
+# monitoraPasta "$pastaBh" &
+monitoraPasta "$pastaBh"
+
